a9208f5083fa8cc139d96f13c1b5df8248f46e7e
Max <max181199@LAPTOP-QRSLDOD6.localdomain>
2022-10-24 19:23:47 +0300

    done

--- p2_par.c
+++ p2_par.c
@@ -1,4 +1,6 @@
-#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 #include <time.h>
 #include <mpi.h>
 
@@ -7,13 +9,13 @@
 #define V 16
 #define B -123
 
-using namespace std;
+// struct Point {
+//   double x;
+//   double y;
+//   double z;
+// };
 
-struct Point {
-  double x;
-  double y;
-  double z;
-};
+typedef struct { double x, y, z; } Point;
 
 void fill(Point* p, long len, int size) {
   int diff = N % size > 0 ? size - N % size : 0;
@@ -23,11 +25,11 @@ void fill(Point* p, long len, int size) {
       p[i].x = 0;
       p[i].y = 0;
       p[i].z = 0;
-      //("Y,");
+      //printf("Y,");
     } else {
-      p[i].x = 2 * (static_cast<double>(std::rand()) / RAND_MAX) - 1;
-      p[i].y = 2 * (static_cast<double>(std::rand()) / RAND_MAX) - 1;
-      p[i].z = 4 * (static_cast<double>(std::rand()) / RAND_MAX) - 2;
+      p[i].x = 2 * ((double)(rand()) / RAND_MAX) - 1;
+      p[i].y = 2 * ((double)(rand()) / RAND_MAX) - 1;
+      p[i].z = 4 * ((double)(rand()) / RAND_MAX) - 2;
       //printf("X,");
     }
   }
@@ -35,9 +37,9 @@ void fill(Point* p, long len, int size) {
   return;
 }
 
-double F(Point& p) {
-  if ( ((abs(p.x) + abs(p.y)) <= 1) && (-2 <= p.z) && (p.z <= 2))
-    return p.x * p.x * p.y * p.y * p.z * p.z;
+double F(Point * p) {
+  if ( ((fabs(p -> x) + fabs(p -> y)) <= 1) && (-2 <= p -> z) && (p -> z <= 2))
+    return p->x * p->x * p->y * p->y * p->z * p->z;
   return 0;
 }
 
@@ -78,7 +80,6 @@ int main(int argc, char* argv[]) {
     if (!argv[1]) return 1;
     sscanf(argv[1], "%lf", &epsilon);
     
-    
     Point p[len];
     
     int scale = 1;
@@ -87,7 +88,8 @@ int main(int argc, char* argv[]) {
 
     fill(p, len, slave);
 
-    while (abs(I - integral) > epsilon) {
+
+    while (fabs(16.0 / 135 - integral) > epsilon) {
       MPI_Scatterv(p, sendcounts, displs, MPI_DOUBLE, NULL, 0, MPI_DOUBLE, 0, MPI_COMM_WORLD); 
       fill(p, len, slave);
       double tmp_summ;
@@ -104,20 +106,20 @@ int main(int argc, char* argv[]) {
     
     MPI_Scatterv(p, sendcounts, displs, MPI_DOUBLE, NULL, 0, MPI_DOUBLE, 0, MPI_COMM_WORLD); 
 
-    printf("RES=%.10lf, DEF=%.10lf,  NUM=%d;\n", integral, abs(I- integral), (scale - 1) * N );
+    printf("RES=%.10lf, DEF=%.10lf,  NUM=%d;\n", integral, fabs(I - integral), (scale - 1) * N );
   
   } else {
     int slave = size - 1;
     int len = N + (N % slave > 0 ? slave - N % slave : 0);
     int slave_len = len / slave;
     Point p[slave_len];
-    while (true){
+    while (1){
       MPI_Scatterv(NULL, NULL, NULL, MPI_DOUBLE, p, slave_len * 3, MPI_DOUBLE, 0, MPI_COMM_WORLD); 
       if (p[0].x == B)
         break;
       double summ = 0;
       for (long i = 0; i < slave_len; i++){
-        summ += F(p[i]);
+        summ += F(p + i);
       }
       MPI_Reduce(&summ, NULL, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);  
     }
